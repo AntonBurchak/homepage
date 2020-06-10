1579c8848b30d726e4c8dbcfebb98bccca0b3abc
Anton Burchak <anton.burchak01@gmail.com>
2020-06-10 16:40:30 +0300

    rel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 
+import {Controller} from './assets/js/Controller';
 
+import './assets/js/Transition'
 
+import './assets/scss/index.scss'
 
+import 'normalize.css'
 
+import './assets/fonts/_fonts.scss';
+
+// Controller.setHash('main');
+// console.log(Controller.getHash());
